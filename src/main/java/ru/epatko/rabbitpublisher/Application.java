74f72e9d3b46b215ed8258bcfbabcbb665cb0a79
epatko <mikhail.epatko@gmail.com>
2018-11-20 11:29:16 +0300

    Add repo

--- src/main/java/ru/epatko/rabbitpublisher/Application.java
+++ src/main/java/ru/epatko/rabbitpublisher/Application.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class RabbitConnectorApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RabbitConnectorApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
